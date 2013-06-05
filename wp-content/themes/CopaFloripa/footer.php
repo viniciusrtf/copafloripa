34d92009b6e256548f0c543dcd16936f82109638
Vinícius R. T. Ferraz <viniciusrtf@gmail.com>
2013-06-05 15:38:29 -0300

    Copa Floripa Hotsite 1st ver. Still lacks google map and the form must be tweaked

---
+++ wp-content/themes/CopaFloripa/footer.php
@@ -0,0 +1,13 @@
+	<footer id="main-footer">
+	<?php get_sidebar( 'footer' ); ?>
+
+		<div id="footer-bottom">
+			<div class="container">
+				<p id="copyright">&copy;2013 GoSports Brazil - Alguns direitos reservados.<br/>
+				Site desenvolvido por <a href="http://www.zemplo.com.br" target="_blank">ZEMPLO</a>, orgulhosamente utilizando <a href="http://br.wordpress.org" target="_blank">WordPress</a>.</p>
+			</div> <!-- end .container -->
+		</div> <!-- end #footer-bottom -->
+	</footer> <!-- end #main-footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
